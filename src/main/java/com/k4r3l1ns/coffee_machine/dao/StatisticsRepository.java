71347ec645cc14c09f5a02c1c421910deaa4db55
k4r3l1ns <makcum.kyp4ehkob@gmail.com>
2024-09-06 18:42:49 +0300

    #3 Added Repository layer & Integrated MapStruct

---
+++ src/main/java/com/k4r3l1ns/coffee_machine/dao/StatisticsRepository.java
@@ -0,0 +1,45 @@
+package com.k4r3l1ns.coffee_machine.dao;
+
+import com.k4r3l1ns.coffee_machine.models.Statistics;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StatisticsRepository extends JpaRepository<Statistics, Long> {
+
+    @Query(
+            """
+                SELECT S
+                FROM Statistics S
+                WHERE S.orderCount > 0
+                ORDER BY S.orderCount DESC
+                LIMIT :count
+            """
+    )
+    List<Statistics> mostOrderedCoffee(int count);
+
+    @Query(
+            """
+                SELECT S
+                FROM Statistics S
+                WHERE S.overallRevenue > 0
+                ORDER BY S.overallRevenue DESC
+                LIMIT :count
+            """
+    )
+    List<Statistics> mostProfitableCoffee(int count);
+
+    @Query(
+            """
+                SELECT S
+                FROM Statistics S
+                WHERE S.portionCount > 0
+                ORDER BY S.portionCount DESC
+                LIMIT :count
+            """
+    )
+    List<Statistics> mostServedCoffee(int count);
+}
